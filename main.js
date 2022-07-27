f53a7e5038d84366f06e352db8af92199f4c7aeb
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2022-07-26 22:21:28 -0300

    Segunda-Entrega

--- main.js
+++ main.js
@@ -1,133 +1,127 @@
-class Usuario{
-    constructor(nombre, email, constraseña, capital, activo, precioC, precioV, apalancamiento){
-        this.nombre = nombre
-        this.email = email 
-        this.constraseña = constraseña
-        this.capital = capital
-        this.activo = activo
-        this.precioC = precioC
-        this.precioV = precioV
-        this.apalancamiento = apalancamiento
-        
-        
+class Usuario {
+    constructor(nombre, email, constraseña, capital, activo, precioC, precioV, apalancamiento) {
+        this.nombre = nombre;
+        this.email = email;
+        this.constraseña = constraseña;
+        this.capital = capital;
+        this.activo = activo;
+        this.precioC = precioC;
+        this.precioV = precioV;
+        this.apalancamiento = apalancamiento;
+
+
     }
-  /*operacion() {
-    return ((this.precioV-this.precioC)/this.precioC)*100
-  }*/
+    /*operacion() {
+      return ((this.precioV-this.precioC)/this.precioC)*100
+    }*/
 }
-let usuarios = []
- 
+let usuarios = [];
 
-if(localStorage.getItem("usuarios")){
-    usuarios = JSON.parse(localStorage.getItem("usuarios"))
-}else{
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+
+if (localStorage.getItem("usuarios")) {
+    usuarios = JSON.parse(localStorage.getItem("usuarios"));
+} else {
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 }
 
 
-const form = document.getElementById("idForm")
-const botonUsuarios = document.getElementById("botonUsuarios")
-const divUsuarios = document.getElementById("divUsuarios")
-const botonGanancia = document.getElementById("botonGanancia")
-const divGanancia = document.getElementById("divGanancia")
+const form = document.getElementById("idForm");
+const botonUsuarios = document.getElementById("botonUsuarios");
+const divUsuarios = document.getElementById("divUsuarios");
+const botonGanancia = document.getElementById("botonGanancia");
+const divGanancia = document.getElementById("divGanancia");
 
 
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault()
-    console.log(e.target)
-    let datForm = new FormData(e.target)
-    let usuario = new Usuario(datForm.get("nombre"), datForm.get("email"), datForm.get("constraseña"), datForm.get("capital"), datForm.get("activo"), datForm.get("PrecioC"), datForm.get("PrecioV"), datForm.get("PrecioV"), datForm.get("apalancamiento"))
-    usuarios.push(usuario)
-    console.log(usuarios)
-    
-    
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    let datForm = new FormData(e.target);
+    let usuario = new Usuario(datForm.get("nombre"), datForm.get("email"), datForm.get("constraseña"), datForm.get("capital"), datForm.get("activo"), datForm.get("PrecioC"), datForm.get("PrecioV"), datForm.get("PrecioV"), datForm.get("apalancamiento"));
+    usuarios.push(usuario);
+    console.log(usuarios);
+
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
     form.reset();
 
-     
-    
+
+
 })
 
 
 botonUsuarios.addEventListener("click", () => {
-    let arrayStorage = JSON.parse(localStorage.getItem("usuarios"))
-divUsuarios.innerHTML = ""
-arrayStorage.forEach((usuario, indice) => {
-    divUsuarios.innerHTML +=  `
+    let arrayStorage = JSON.parse(localStorage.getItem("usuarios"));
+    divUsuarios.innerHTML = ""
+    arrayStorage.forEach((usuario, indice) => {
+        divUsuarios.innerHTML += `
     <div class="card border-dark mb-3" id="usuario${indice}" style="max-width: 20rem; margin: 4px;">
-        <div class="card-header"><h2>${usuario.nombre}</h2></div>
+        <div class="card-header"><h2>Bienvenido ${usuario.nombre}</h2></div>
         <div class="card-body">
-            <p class="card-title">${usuario.email}</p>
+            <p class="card-title">Su margen es de: ${usuario.capital}$</p>
             <button class="btn btn-danger">Eliminar Tarea</button>
         </div>
     </div>
     
     `
-})
-
-arrayStorage.forEach((usuario, indice) => {
-    let botonCard = document.getElementById(`usuario${indice}`).lastElementChild.lastElementChild
-    botonCard.addEventListener("click", () => {
-        document.getElementById(`usuario${indice}`).remove()
-        usuarios.splice(indice,1)
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
-console.log (`${usuario.nombre} Eliminada`)   
-
     })
-})
+
+    arrayStorage.forEach((usuario, indice) => {
+        let botonCard = document.getElementById(`usuario${indice}`).lastElementChild.lastElementChild;
+        botonCard.addEventListener("click", () => {
+            document.getElementById(`usuario${indice}`).remove();
+            usuarios.splice(indice, 1);
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            console.log(`${usuario.nombre} Eliminada`);
+
+        })
+    })
 
 })
 
 ///FUNCION PARA CALCULAR LA GANANCIA 
-function porcentaje(precioC, precioV){
-    return ((precioV-precioC)/precioC)*100;
-    
-   }
-   function resultado (porcentaje, apalancamiento, capital){
-    return ((porcentaje*apalancamiento)*capital)/100
-   }
+function porcentaje(precioC, precioV) {
+    return ((precioV - precioC) / precioC)*100;
+
+}
+function resultado(porcentaje, apalancamiento, capital) {
+    return ((porcentaje * apalancamiento) * capital) / 100;
+}
 
 
 
 botonGanancia.addEventListener("click", () => {
 
-    let arrayResultado = JSON.parse(localStorage.getItem("usuarios"))
+    let arrayResultado = JSON.parse(localStorage.getItem("usuarios"));
     divGanancia.innerHTML = ""
     arrayResultado.forEach((usuario, indice) => {
-        divGanancia.innerHTML =   `
+   let porcentajeGanancia = porcentaje(usuario.precioC,usuario.precioV);
+        divGanancia.innerHTML = `
         <div class="card border-dark mb-3" id="usuario${indice}" style="max-width: 20rem; margin: 4px;">
-            <div class="card-header"><h2>${usuario.nombre}</h2></div>
+            <div class="card-header"><h4>Su ganancia en ${usuario.activo} es:</h4></div>
             <div class="card-body">
-                <p class="card-title">${resultado(usuario)}</p>
+                <p class="card-title">+${resultado(porcentajeGanancia, usuario.apalancamiento, usuario.capital)}$</p>
                 
             </div>
         </div>
         
         `
 
+    });
+
+})
+
+    arrayResultado.forEach((usuario, indice) => {
+        let botonBorrar = document.getElementById(`usuario${indice}`).lastElementChild.lastElementChild;
+        botonBorrar.addEventListener("click", () => {
+            document.getElementById(`usuario${indice}`).remove();
+            usuarios.splice(indice, 1);
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            console.log(`${usuario.nombre} Eliminada`);
+        })
     })
 
 
 
-arrayResultado.forEach((usuario, indice) => {
-    let botonBorrar = document.getElementById(`usuario${indice}`).lastElementChild.lastElementChild
-    botonBorrar.addEventListener("click", () => {
-        document.getElementById(`usuario${indice}`).remove()
-        usuarios.splice(indice,1)
-        localStorage.setItem("usuarios", JSON.stringify(usuarios))
-        console.log(`${usuario.nombre} Eliminada`)
-    })
-})
-
-})
-//calcular ganancia total:
-//(porcentaje*usuario.apalancamiento)*usuario.capital)/100
 
 
-
-//calcular porcentaje:
-//((usuario.precioV-usuario.precioC)/usuario.precioC)*100
-
-//Boton de borrar:
-//<button class="btn btn-danger">Eliminar Tarea</button>
